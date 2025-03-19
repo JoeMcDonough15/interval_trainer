e39f7d3565f0e96b66608da924730ff397db752d
joemcdonough15 <joemcdonoughmusic@gmail.com>
2025-03-18 21:47:12 -0400

    Initial app layout

---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,16 @@
+import { StackLayout } from "@progress/kendo-react-layout";
+
+const App = () => {
+  return (
+    <main>
+      <StackLayout className="main-container">
+        <h1>Interval Trainer</h1>
+        <p>Interval Player</p>
+        <p>User Select Interval</p>
+        <p>Interval Options</p>
+      </StackLayout>
+    </main>
+  );
+};
+
+export default App;
