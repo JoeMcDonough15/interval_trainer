154e08cdb4aeca1ea76e6079be03bec2fcefa28a
joemcdonough15 <joemcdonoughmusic@gmail.com>
2025-03-20 00:24:56 -0400

    Begin styling StatsDashboard

--- frontend/src/components/StatsDashboard/StatsDashboard.tsx
+++ frontend/src/components/StatsDashboard/StatsDashboard.tsx
@@ -1,15 +1,21 @@
 import { useContext } from "react";
 import { UserStatsContext } from "../../context/UserStats";
 import { Card } from "@progress/kendo-react-layout";
+import "./StatsDashboard.css";
 
 const StatsDashboard = () => {
   const { numCorrect, totalNumAnswered } = useContext(UserStatsContext);
   return (
-    <section className="dashboard">
-      <Card>
-        <span>{numCorrect}</span> / <span>{totalNumAnswered}</span>
+    <section className="row dashboard-container">
+      <Card className="row dashboard-card">
+        <div className="row num-correct-out-of-total">
+          <span>{numCorrect}</span>
+          <span>/</span>
+          <span>{totalNumAnswered}</span>
+        </div>
+
         {totalNumAnswered > 0 && (
-          <p>
+          <p className="average">
             Your average is {Math.round((numCorrect / totalNumAnswered) * 100)}%
           </p>
         )}
