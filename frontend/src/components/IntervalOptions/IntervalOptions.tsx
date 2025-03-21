47cf3c59f2d0352ffe8c0335c1ec356e1f9dcc52
joemcdonough15 <joemcdonoughmusic@gmail.com>
2025-03-21 02:00:16 -0400

    Begin larger screen styles for IntervalOptions

--- frontend/src/components/IntervalOptions/IntervalOptions.tsx
+++ frontend/src/components/IntervalOptions/IntervalOptions.tsx
@@ -83,44 +83,47 @@ const IntervalOptions = () => {
         }`}
       >
         <form onSubmit={handleSubmit}>
-          <fieldset>
-            <EmptyInputsError
-              errorObj={intervalOptionsError}
-              setErrorObj={setIntervalOptionsError}
-              specificError="noIntervalsIncluded"
-            />
-            <legend>Intervals To Include</legend>
-            <div className="row user-select-inputs">
-              {Object.keys(availableIntervals).map((interval) => {
-                return (
-                  <ToggleIntervalCheckbox
-                    key={interval}
-                    intervalName={interval}
-                    setIntervals={setIntervals}
-                  />
-                );
-              })}
-            </div>
-          </fieldset>
-          <fieldset>
-            <legend>Directions To Include</legend>
-            <EmptyInputsError
-              errorObj={intervalOptionsError}
-              setErrorObj={setIntervalOptionsError}
-              specificError="noDirectionsIncluded"
-            />
-            <div className="row user-select-inputs">
-              {Object.keys(availableDirections).map((direction) => {
-                return (
-                  <ToggleDirectionCheckbox
-                    key={direction}
-                    directionName={direction}
-                    setDirections={setDirections}
-                  />
-                );
-              })}
-            </div>
-          </fieldset>
+          <div className="intervals-and-directions-container">
+            <fieldset className="intervals-included-section">
+              <EmptyInputsError
+                errorObj={intervalOptionsError}
+                setErrorObj={setIntervalOptionsError}
+                specificError="noIntervalsIncluded"
+              />
+              <legend>Intervals To Include</legend>
+              <div className="row user-select-inputs">
+                {Object.keys(availableIntervals).map((interval) => {
+                  return (
+                    <ToggleIntervalCheckbox
+                      key={interval}
+                      intervalName={interval}
+                      setIntervals={setIntervals}
+                    />
+                  );
+                })}
+              </div>
+            </fieldset>
+            <fieldset className="directions-included-section">
+              <legend>Directions To Include</legend>
+              <EmptyInputsError
+                errorObj={intervalOptionsError}
+                setErrorObj={setIntervalOptionsError}
+                specificError="noDirectionsIncluded"
+              />
+              <div className="row user-select-inputs">
+                {Object.keys(availableDirections).map((direction) => {
+                  return (
+                    <ToggleDirectionCheckbox
+                      key={direction}
+                      directionName={direction}
+                      setDirections={setDirections}
+                    />
+                  );
+                })}
+              </div>
+            </fieldset>
+          </div>
+
           <Button>Apply Changes</Button>
         </form>
       </div>
